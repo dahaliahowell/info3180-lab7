90222088bb2502126af2194b675d6ecdacc51f11
dahaliahowell <dahaliahowell.dh@gmail.com>
2021-04-11 05:22:44 -0500

    creadted front-ennd using vue js

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
 
 class UploadForm(FlaskForm):
     photo = FileField('Photo', validators=[
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,6 +8,81 @@ const app = Vue.createApp({
     }
 });
 
+const uploadform = {
+    name: 'upload-form',
+    template: `
+    <h1 id="form-heading">Upload Form</h1>
+
+    <div class="alert alert-success" v-if="isSuccess">
+        <p v-for="success in successMessage">{{ success.message }}</p>
+    </div>
+    <div class="alert alert-danger" v-if="isError">
+        <ul>
+            <li v-for="error in errors">{{ error }}</li>
+        </ul>
+    </div>
+
+    <form method="POST" action="" id="uploadForm" @submit.prevent="uploadPhoto">
+
+        <div class="form-group">
+            <label class="form-label" for="description">Description</label><br>
+            <textarea name="description" id="description" class="form-element"></textarea><br>
+            <label class="form-label" for="photo">Photo</label><br>
+            <input type="file" id="photo" class="form-element" name="photo" accept="image/x-png,image/jpg">
+        </div>
+
+        <button type="submit" name="submit" class="btn btn-primary">Upload</button>
+    
+    </form>
+    `,
+    data: function() {
+        return {
+            errors: [],
+            successMessage: [],
+            isSuccess: false,
+            isError: false
+        };
+    },
+    methods: {
+        uploadPhoto: function() {
+            let self = this;
+            let uploadForm = document.getElementById('uploadForm');
+            let form_data = new FormData(uploadForm);
+
+            fetch("/api/upload", {
+                method: 'POST',
+                body: form_data,
+                headers: {
+                    'X-CSRFToken': token
+                },
+                credentials: 'same-origin'        
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonResponse) {
+                console.log('success');
+                console.log(jsonResponse);
+
+                if (jsonResponse.errors){
+                    self.isError= true;
+                    self.isSuccess = false;
+                    self.errors = jsonResponse.errors;
+                } else if (jsonResponse.upload){
+                    self.isError = true;
+                    self.isSuccess= false;
+                    self.successMessage = jsonResponse.upload;
+                }
+
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    
+        }
+    }
+};
+
 app.component('app-header', {
     name: 'AppHeader',
     template: `
@@ -21,6 +96,9 @@ app.component('app-header', {
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/upload">Upload </router-link>
           </li>
         </ul>
       </div>
@@ -73,7 +151,7 @@ const NotFound = {
 const routes = [
     { path: "/", component: Home },
     // Put other routes here
-
+    {path: "/upload", component: uploadform},
     // This is a catch all route in case none of the above matches
     { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
 ];
